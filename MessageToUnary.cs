b7966774f770727718a1c3ecff9d336e450f7b01
Pedro Ramos <ramosct70@hotmail.com>
2020-02-05 12:45:40 -0300

    Bug Fixed

--- MessageToUnary.cs
+++ MessageToUnary.cs
@@ -7,40 +7,42 @@ using System.Collections.Generic;
 
 class Solution {
     public static string ConverterBinario(string mensagem) {
-        string resultado = mensagem;
         string stringBinaria = "";
 
         foreach (char letra in mensagem) {
-            int codigoNum = (int)letra;
-            stringBinaria += Convert.ToString(codigoNum, 2);
+            string caracterBinario = "";
+            caracterBinario += "0" + Convert.ToString(letra, 2);
+            if (caracterBinario.Length == 8) {
+                caracterBinario = caracterBinario.Substring(1, 7);
+            }
+            stringBinaria += caracterBinario;
         }
 
-        resultado = stringBinaria;
-        return resultado;
+        return stringBinaria;
     }
 
     public static string ConverterUnario(string binario, string mensagem) {
-        string stringBinaria = "";
+        string stringUnaria = "";
         int index = 0;
         int contagem1 = 0;
         int contagem0 = 0;
 
-        foreach (char numero in binario) { // binario = 1000011
+        foreach (char numero in binario) {
             index += 1;
             if (numero == '1' && contagem0 == 0) {
                 contagem1 += 1;
-                if (index == (binario.Length / mensagem.Length) && contagem1 == 1) {
+                if (index == binario.Length && contagem1 == 1) {
                     string zeros = new String('0', contagem0);
-                    stringBinaria += "0 " + zeros;
+                    stringUnaria += "0 " + zeros;
                     contagem1 = 0;
                     index = 0;
                 }
             }
             if (numero == '0' && contagem1 == 0) {
                 contagem0 += 1;
-                if (index == (binario.Length / mensagem.Length) && contagem0 == 1) {
+                if (index == binario.Length && contagem0 == 1) {
                     string zeros = new String('0', contagem0);
-                    stringBinaria += "00 " + zeros;
+                    stringUnaria += "00 " + zeros;
                     contagem0 = 0;
                     index = 0;
                 }
@@ -48,38 +50,45 @@ class Solution {
             if (numero == '1' && contagem0 != 0) {
                 contagem1 += 1;
                 string zeros = new String('0', contagem0);
-                stringBinaria += "00 " + zeros + " ";
+                stringUnaria += "00 " + zeros + " ";
                 contagem0 = 0;
 
             }
             if (numero == '0' && contagem1 != 0) {
                 contagem0 += 1;
                 string zeros = new String('0', contagem1);
-                stringBinaria += "0 " + zeros + " ";
+                stringUnaria += "0 " + zeros + " ";
                 contagem1 = 0;
             }
-            if (index == (binario.Length / mensagem.Length) && contagem1 != 0) {
+            if (index == binario.Length && contagem1 != 0) {
                 string zeros = new String('0', contagem1);
-                stringBinaria += "0 " + zeros;
+                stringUnaria += "0 " + zeros;
                 contagem1 = 0;
                 index = 0;
             }
-            if (index == (binario.Length / mensagem.Length) && contagem0 != 0) {
+            if (index == binario.Length && contagem0 != 0) {
                 string zeros = new String('0', contagem0);
-                stringBinaria += "00 " + zeros;
+                stringUnaria += "00 " + zeros;
                 contagem0 = 0;
                 index = 0;
             }
         }
 
-        return stringBinaria;
+        return stringUnaria;
     }
 
     static void Main(string[] args) {
-        string MESSAGE = Console.ReadLine();
-        string binario = ConverterBinario(MESSAGE);
-        string unario = ConverterUnario(binario, MESSAGE);
+        try {
+            string MESSAGE = Console.ReadLine();
+            string binario = ConverterBinario(MESSAGE);
+            string unario = ConverterUnario(binario, MESSAGE);
 
-        Console.WriteLine(unario);
+            Console.WriteLine(binario);
+            Console.WriteLine(unario);
+            Console.WriteLine("0 0 00 0000 0 000 00 0000 0 00");
+        } catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
+        
     }
 }
